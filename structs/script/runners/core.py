9fb6a7aa3b1f5f4b09e535575dced331d718fd4e
kotmarkot <andrey.patrusov@gmail.com>
2025-05-30 18:25:20 +0000

    separated launch from validation

---
+++ structs/script/runners/core.py
@@ -0,0 +1,24 @@
+from abc import abstractmethod, ABC
+from pathlib import Path
+from typing import Optional, Any
+from utils import normalize_path, validate_path
+import os
+from subprocess import Popen
+
+
+class ScriptRunner(ABC):
+    def __init__(self) -> None:
+        # script working directory path
+        self.cwd: Optional[Path] = None
+        # TODO: Добавить дополнительные праметры и сетеры
+        ## path to stdin log file
+        # self.stdin: Optional[Path] = None
+
+    def set_cwd(self, path: Path | str):
+        path = normalize_path(path)
+        validate_path(path, permissions=os.W_OK + os.R_OK + os.X_OK, is_file=False)
+
+    @abstractmethod
+    def run(self) -> Popen[Any]:
+        # TODO: Подумать над return
+        pass
