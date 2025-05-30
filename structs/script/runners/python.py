ead656627bff6661eedf9d5248e96d1c8e0da0ed
kotmarkot <andrey.patrusov@gmail.com>
2025-05-30 19:55:42 +0000

    add PythonRunner

--- structs/script/runners/python.py
+++ structs/script/runners/python.py
@@ -1,1 +1,18 @@
+from core import ScriptRunner
+from typing import Any
+from subprocess import Popen
+from pathlib import Path
 
+
+class PythonRunner(ScriptRunner):
+    def __init__(self, path: Path):
+        super().__init__(path)
+
+        # TODO: Добавить настройки интерпиритатора
+
+    def run(self) -> Popen[Any]:
+        # TODO: Добавить поддержку настроек интерпритатора
+
+        script = Popen(["python", str(self.path)], cwd=self.cwd)
+
+        return script
