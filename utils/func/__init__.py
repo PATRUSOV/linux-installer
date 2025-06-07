3296c4a095aa7b82b3067a408d6b9609ae95a284
kotmarkot <andrey.patrusov@gmail.com>
2025-06-07 16:54:29 +0000

    Now not_none_arg() checks not the first argument, but the specified argument

---
+++ utils/func/__init__.py
@@ -0,0 +1,3 @@
+from .notnone import not_none_arg
+
+__all__ = ["not_none_arg"]
--- utils/func/notnone.py
+++ utils/func/notnone.py
@@ -2,33 +2,21 @@ import inspect
 from functools import wraps
 
 
-def not_none(func):
-    signature = inspect.signature(func)
-    parameters = signature.parameters
-    # TODO: Добавить обработку ситуации если пользователь использует
-    # нестандартные именно вместо cls или self
+def not_none_arg(arg_name):
+    def decorator(func):
+        signature = inspect.signature(func)
 
-    if not parameters or (
-        ("self" in parameters or "cls" in parameters) and len(parameters) == 1
-    ):
-        raise ValueError(f"Функция {func.__name__} не имеет аргументов")
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            bound = signature.bind(*args, **kwargs)
+            bound.apply_defaults()
 
-    param_iter = iter(parameters)
+            # TODO: Добавить проверку есть ли у функции аргмент arg_name
+            if bound.arguments[arg_name] is None:
+                return None
 
-    if "self" in parameters or "cls" in parameters:
-        next(param_iter)
-    first_arg_name = next(param_iter)
-
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        bound = signature.bind(*args, **kwargs)
-        bound.apply_defaults()
-
-        value = bound.arguments[first_arg_name]
-
-        if value is not None:
             return func(*args, **kwargs)
 
-        return None
+        return wrapper
 
-    return wrapper
+    return decorator
