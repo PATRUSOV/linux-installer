c7082894556d0e23b6e3856166e604b9ec5e8b4d
kotmarkot <andrey.patrusov@gmail.com>
2025-06-07 19:11:58 +0000

    add comms

--- utils/file/backup.py
+++ utils/file/backup.py
@@ -2,20 +2,25 @@ from pathlib import Path
 import os
 from utils.path import get_safe_path
 
+# ПРИ ПЕРЕИМЕНОВАНИИ ВО ИЗБЕЖАНИИ БАГОВ НУЖЕН АБСОЛЮТНЫЙ ПУТЬ, get_safe_path() это обеспечивает
+# FIXME: УБрать валидацию из backup и ubackup
+
 
 def backup(path: Path | str) -> None:
+    # TODO: Добавить доки
     path = get_safe_path(path, permissions=os.W_OK)
 
     if path.suffix == ".bak":
         raise ValueError(f"{path.name} уже являеться бекапом")
 
-    path = path.with_name(path.name + ".bak")
+    path = path.rename(str(path) + ".bak")
 
 
 def ubackup(path: Path | str) -> None:
+    # TODO: Добавить доки
     path = get_safe_path(path, permissions=os.W_OK)
 
     if path.suffix == ".bak":
-        path = path.with_name(path.stem)
+        path = path.rename(str(path)[0:-4])
     else:
         raise ValueError(f"{path.name} не являеться бекапом")
--- utils/file/copy.py
+++ utils/file/copy.py
@@ -7,6 +7,7 @@ from utils.path import get_safe_path
 
 
 def copy(src: Path | str, dst: Path | str):
+    # TODO: Добавить доки
     src = get_safe_path(src, permissions=os.R_OK)
     dst = get_safe_path(dst, permissions=os.X_OK + os.W_OK, is_file=False)
 
