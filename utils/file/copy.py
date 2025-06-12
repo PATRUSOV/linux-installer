6b6fa77dd562a7a5e02bd1cfd26e6a39634e0a83
kotmarkot <andrey.patrusov@gmail.com>
2025-06-12 10:48:15 +0000

    add flag raw + docs

--- utils/file/copy.py
+++ utils/file/copy.py
@@ -1,15 +1,20 @@
 import shutil
 from pathlib import Path
 import os
-
-
 from utils.path import get_safe_path
 
 
-def copy(src: Path | str, dst: Path | str):
-    # TODO: Добавить доки
-    src = get_safe_path(src, permissions=os.R_OK)
-    dst = get_safe_path(dst, permissions=os.X_OK + os.W_OK, is_file=False)
+def copy(src: Path | str, dst: Path | str, raw: bool = False):
+    """Утилита для копирования, работает и с файлами, и с директориями
+    args:
+        src: Путь к копируемому
+        dst: Куда копировать (обязатольно директоория)
+        raw: Валидиривать ли аргументы
+    """
+
+    if not raw:
+        src = get_safe_path(src, permissions=os.R_OK)
+        dst = get_safe_path(dst, permissions=os.X_OK + os.W_OK, is_file=False)
 
     if src.is_dir():
         shutil.copytree(src, dst)
