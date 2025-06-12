5b632fe87f3a02e8eefd3b2327b4c6364a90e927
kotmarkot <andrey.patrusov@gmail.com>
2025-06-12 11:42:44 +0000

    Now with Exception in backup() you continue or terminate the programme

--- structs/config.py
+++ structs/config.py
@@ -1,4 +1,5 @@
-from utils.path import backup, normalize_path, validate_path, copy
+from utils.path import get_safe_path
+from utils.file import backup, copy
 from pathlib import Path
 from core import Installable
 import os
@@ -10,24 +11,24 @@ class Config(Installable):
         self, source: str | Path, path: str | Path, backup: bool = True
     ) -> None:
         # TODO: Написать доки
-        source = normalize_path(source)
-        validate_path(source, permissions=os.R_OK, is_file=None)
-        self.source: Path = source
+        self.source: Path = get_safe_path(source, permissions=os.R_OK, is_file=None)
 
-        path = normalize_path(path)
-        validate_path(path, permissions=os.W_OK, is_file=False)
-        self.path: Path = path
+        self.path: Path = get_safe_path(path, permissions=os.W_OK, is_file=False)
 
         self.backup: bool = backup
 
     def install(self) -> None:
         if self.backup:
-            # TODO: ИСПРАВИТЬ ЭТУ ХРЕНЬ
             try:
                 backup(self.path / self.source.name)
-            except FileNotFoundError:
-                pass
-
+            except Exception as e:
+                print(f"При бекапе существующией конфигурации возникла ошибка: {e}")
+                while True:
+                    answer = input("Продолжать устнановку? (y, n)").upper()
+                    if answer == "N":
+                        raise RuntimeError(f"Программа завершена с ошибкой {e}")
+                    elif answer == "Y":
+                        break
         try:
             copy(self.source, self.path)
         except Exception as e:
